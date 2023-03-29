44c9939fb4d117d10c6143b97b9e5ec39b8cad43
Nagaraj Valleru <nagaraj.valleru@gmail.com>
2023-03-28 21:46:54 -0500

    Added the python exercises

---
+++ Exercise6.3_March27th_assignment.py
@@ -0,0 +1,9 @@
+def main():
+    x = 5
+    y = 2
+    z = Exponential(x,y)
+    print(z)
+    print(z>10)
+def Exponential(x,y):
+    return(x**y)
+main()
---
+++ Exercise7.3_March28th_assignment.py
@@ -0,0 +1,2 @@
+s = "pythonclass"
+print(s[0:3] + s[-3:])
